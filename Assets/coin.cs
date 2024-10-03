6850b33dfc1ebd4a838eb75164b0585deb1da3c8
AmandaBoard <boar9227@vandals.uidaho.edu>
2024-10-02 22:08:14 -0700

    update coin.cs

--- Assets/coin.cs
+++ Assets/coin.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class coin : MonoBehaviour
 {
+    public GameObject gameManagerObject;
+    public GameManager gameManager;
+    public AudioManager audioManager;
+
+    //private void Awake(){
+    // audioManager = GameObject.FindGameObjectWithTag("Audiotag");
+    //}
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManagerObject = gameManagerObject.FindWithTag("GameController");
+        if(gameManagerObject != null){
+            gameManager = gameManagerObject.GetComponent<gameManager>();
+        } else {
+            Debug.LogError("Game Manager not found");
+        }        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            if(gameManager != null){
+                gameManager.AddScore(coinValue);
+                gameObject.SetActive(false);
+                //add coin sound effect
+                //audioManager.PlaySFX(audioManager.coinSFX)
+            }
+        } else {
+
+        }
+    }   
 }
