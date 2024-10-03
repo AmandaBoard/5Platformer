45a8fb65dd8649a82eaef1c5e545bd2db0f37ec1
AmandaBoard <boar9227@vandals.uidaho.edu>
2024-10-02 22:42:44 -0700

    updated parrallax

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-
-
     private float length, startpos, posY=2.5f;
     public GameObject cam;
     public float parallaxEffect;
-
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startpos=transform.position.x;
+        length=GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position= new Vector3(startpos+dist, posY,transform.position.z);
-        if(temp > startpos+length) startpos+=length;
-        else if (temp<startpos-length)startpos-=length;
+        float temp = (cam.transform.position.x *(1-parallaxEffect));
+        float dist=(cam.transform.position.x*parallaxEffect);
+        //Using this line will make the background track the player moves as they move up and down
+        // transform.position=new Vector3(startpos+dist, transform.position.y,transform.position.z);
+        //Using this line will make the background not track the player as they move up and down
+        transform.position=new Vector3(startpos+dist,posY,transform.position.z);
+        if(temp>startpos+length)startpos+=length*2;
+        else if(temp<startpos-length)startpos-=length*2;
     }
 }
