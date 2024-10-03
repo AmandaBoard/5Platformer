97bc81e2c29b7a6d65b693aa6331955d14efb9b0
AmandaBoard <boar9227@vandals.uidaho.edu>
2024-10-02 22:52:53 -0700

    camera follow script

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;     // Reference to the player's transform
+    public Vector3 offset;       // Offset between the camera and player
+    public float smoothSpeed = 0.125f; // Smooth movement speed
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // Calculate the desired position of the camera
+        Vector3 desiredPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+
+        // Smoothly move the camera towards the desired position
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // Update the camera's position
+        transform.position = smoothedPosition;
     }
 }
